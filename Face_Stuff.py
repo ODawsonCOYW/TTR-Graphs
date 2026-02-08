f2d5d67592710321222e3d223c6fc9cc4e7bdced
ODawsonCOYW <oliverjd2005@gmail.com>
2026-02-09 11:48:31 +1300

    Removed Alg Calls

--- Face_Stuff.py
+++ Face_Stuff.py
@@ -154,25 +154,3 @@ def face_degree_counts(faces):
         counts.append(c)
     return counts
 
-# G = nx.Graph(adj_list_USA)
-# pos = nx.planar_layout(G)
-    
-# rot_London = make_rotation_system(adj_list_USA, pos)
-
-# face_list = find_faces(rot_London)
-
-# print(face_list)
-# print(face_info(adj_list_USA, adj_list_USA_W, pos))
-#plot_face_degree_histogram(face_list, title = "Face Degree Distribution (Pennsylvania)")
-
-
-    
-
-# face_plots(adj_list_USA, adj_list_USA_W, "USA")
-# face_plots(adj_list_EU, adj_list_EU_W, "EU")
-# face_plots(adj_list_GER, adj_list_GER_W, "Germany")
-# face_plots(adj_list_India, adj_list_India_W, "India")
-# face_plots(adj_list_PEN, adj_list_PEN_W, "Penn")
-# face_plots(adj_list_NY, adj_list_NY_W, "New York")
-# face_plots(adj_list_London, adj_list_London_W, "London")
-
